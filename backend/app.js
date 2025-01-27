e17c03bba0251f2a4a3f661a715ddc31eccfa4fc
J0z3Hp <joseluismarinocuadros@gmail.com>
2025-01-26 22:50:42 -0500

    [creation] creacion del proyecto

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+import express from "express";//ECMA6
+import dotenv from "dotenv";// DEPENDENCIA PARA MANEJAR VARIABLES DE ENTORNO
+import { connectionMongo } from "./src/config/dataBase.js";
+import cors from "cors"
+import { departamentoRouter } from "./src/routes/departamentos.routes.js";
+import { empleadoRouter } from "./src/routes/empleados.routes.js";
+
+// CONFIGURAR EL USO DE NUESTRO SERVIDOR:
+// Dependencia express
+const app = express(); 
+// Dependencia dotenv
+dotenv.config(); // CONFIGURANDO PARA VARIABLES DE ENTORNO. CADA DEPENDENCIA TIENE SU FORMA DE USO, REVISAR DOCUMENTACIÓN. TODAS LAS DEPENDENCIAS SI SE DEBEN IMPORTAR
+// Dependencia mongoose
+connectionMongo();
+const port = process.env.PORT;
+app.use(cors());
+
+// Configutaión de rutas:
+app.use(express.json());
+
+app.use('/departamentos', departamentoRouter);
+app.use('/empleados', empleadoRouter);
+
+
+// EJECUTAR EL SERVIDOR EN NUESTRO COMPUTADOR
+app.listen(port, ()=>{
+    console.log("El servidor esta ejecutandose correctamente, en el puerto", port);
+});
+
